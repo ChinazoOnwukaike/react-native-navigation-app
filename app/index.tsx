bf3444f7d297c4a06f66bd3d83521661f307b9d5
Chinazo <chinazoonwukaike@gmail.com>
2025-02-12 15:42:50 -0600

    get project working again

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,13 @@
-import { View, Text } from "react-native";
+import { View, Text, SafeAreaView } from "react-native";
 import React from "react";
 
 const App = () => {
   return (
-    <View className="mt-10">
-      <Text className="text-3xl font-bold text-blue-500">Hola Mundo</Text>
-    </View>
+    <SafeAreaView>
+      <View className="mt-10 mx-2.5">
+        <Text className="text-3xl font-bold text-blue-500">Hola Mundo</Text>
+      </View>
+    </SafeAreaView>
   );
 };
 
